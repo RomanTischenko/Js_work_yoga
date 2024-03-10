5749b259ca846ab054e8925b7227dbedb3ed3858
roman <romantisenko3@gmail.com>
2024-03-11 00:02:44 +0400

    add calc

--- js/script.js
+++ js/script.js
@@ -213,5 +213,53 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+
+    // CALC
+
+    let persons = document.getElementsByClassName('counter-block-input')[0],
+        days = document.getElementsByClassName('counter-block-input')[1],
+        place = document.getElementById('select'),
+        total = document.getElementById('total'),
+        personsValue = 0,
+        daysValue = 0,
+        totalValue = 0;
+
+    total.textContent = 0;
+
+    persons.addEventListener('change', function() {
+        personsValue = this.value;
+        totalValue = (personsValue + daysValue) * 4000;
+
+        if(personsValue == '') {
+            total.textContent = 0;
+        } else if (daysValue != '' && personsValue == '') {
+            total.textContent = 0;
+        } else if (daysValue != '' && personsValue != '') {
+            total.textContent = totalValue;
+        }
+    });
+
+    days.addEventListener('change', function() {
+        daysValue = this.value;
+        totalValue = (personsValue + daysValue) * 4000;
+
+        if(daysValue == '') {
+            total.textContent = 0;
+        } else if (personsValue != '' && daysValue == '') {
+            total.textContent = 0;
+        } else if (daysValue != '' && personsValue != '') {
+            total.textContent = totalValue;
+        }
+    });
+
+    place.addEventListener('change', function() {
+        if (personsValue == '' || daysValue == '') {
+            total.textContent = 0;
+        } else {
+            let a = totalValue;
+            total.textContent = a * this.options[this.selectedIndex].value;
+        }
+    });
     
 });
