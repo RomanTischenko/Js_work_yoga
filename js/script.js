b0b5c75dc5bcbb56babf2124bb0e9c82e3a32a22
roman <romantisenko3@gmail.com>
2024-03-10 19:33:05 +0400

    add show modal

--- js/script.js
+++ js/script.js
@@ -35,4 +35,32 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+
+    //MODAL
+
+    let btnTab = document.querySelectorAll('.description-btn'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close'),
+        more = document.querySelector('.more');
+
+    btnTab.forEach (function(item) {
+        item.addEventListener('click', function() {
+            overlay.style.display = 'block';
+            this.classList.add('more-splash');
+            document.body.style.overflow = 'hidden';
+
+        });
+    });
+
+    more.addEventListener('click', function() {
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+    });
+
+    close.addEventListener('click', function() {
+        overlay.style.display = 'none';
+        document.body.style.overflow = '';
+    });
 });
